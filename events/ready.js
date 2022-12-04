b69e3159f46f2d30c03a2e522affe10253787e86
Bysimeit <casanosimon@gmail.com>
2022-12-04 06:02:41 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,10 @@
 const Discord = require("discord.js");
 const loadBotStatus = require('../loaders/loadBotStatus');
+const loadRefreshBotStatus = require("../loaders/loadRefreshBotStatus");
 
 module.exports = async bot => {
     await loadBotStatus(bot);
+    await loadRefreshBotStatus(bot);
 
     console.log(`${bot.user.tag} is online !`);
 };
